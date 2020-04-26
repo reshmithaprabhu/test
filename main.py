b30d7e88706c1035a9bb0bd70e1a4a0a351c04ed
Reshmitha <76reshmitha1994@gmail.com>
2020-04-26 17:53:37 +0900

    Added subtraction

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+def sub(a,b):
+    """Calculate the difference of a and b"""
+    return a-b
+
+# displaying the difference
+print(sub(20,10))
+
+
 def add(a,b):
     """Calculate the sum of a and b"""
     return a+b
